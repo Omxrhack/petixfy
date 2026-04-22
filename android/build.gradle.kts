fd852dddeb19916996d575372f9a3967301b77a9
omxrhack <bermejoosuna@gmail.com>
2026-04-21 22:46:33 -0700

    Updated Gradle wrapper to version 8.13 in gradle-wrapper.properties

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 allprojects {
     repositories {
         google()
@@ -17,6 +19,16 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    plugins.withId("com.android.library") {
+        extensions.configure<LibraryExtension>("android") {
+            if (namespace == null) {
+                namespace = "com.petixfy.${project.name.replace('-', '_')}"
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
